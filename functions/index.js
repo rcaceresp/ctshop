7723c1cf86f639f12a197f94b9892580bfbf20aa
Adolfo Gutierrez <adolfo.gutierrez.87@gmail.com>
2020-07-06 18:35:39 -0600

    Send email function added

---
+++ functions/index.js
@@ -0,0 +1,44 @@
+const functions = require('firebase-functions');
+const SparkPost = require('sparkpost');
+const client = new SparkPost('14697761f06f690e2972b9bf681893efc90f94b9');
+const formatMoney = (num) => Number(num).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
+
+exports.sendConfEmail = functions.database.ref('/users/{userId}/orders/{orderId}').onCreate( (snap, ctx) => {
+  const order = snap.val();
+
+  order.products = order.products.map( prod => {
+    const c = {...prod};
+    c['formatted_price'] = `L ${formatMoney(prod.discountedPrice.discountedPrice)}`;
+
+    return c;
+  });
+
+  order['formatted_orderTotal'] = `L ${formatMoney(order.orderTotal)}`;
+
+
+  return client.transmissions.send({
+    options: { sandbox: false },
+    content: {
+      from: 'info@mail.catrachosshop.com',
+      subject: 'Gracias por tu compra!',
+      template_id: 'ctshop-confirmation'
+    },
+    recipients: [{
+      address: order.email,
+      substitution_data: {
+        order: order
+      }
+    }]
+  })
+  .then(data => {
+    console.log(`Confirmation email sent to ${order.email}`);
+
+    return `Confirmation email sent to ${order.email}`;
+  })
+  .catch(err => {
+    console.log(`Whoops! Something went wrong sending confirmation email to ${order.email}`);
+    console.log(err.message);
+
+    return err.message;
+  });
+});
